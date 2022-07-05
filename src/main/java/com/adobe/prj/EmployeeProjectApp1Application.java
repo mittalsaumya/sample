5ea184f94b1889f29bb74cd0920dcdbd5125c444
Saumya Mittal <saumyam@Saumyas-MacBook-Pro.local>
2022-07-05 19:41:16 +0530

    first commit

---
+++ src/main/java/com/adobe/prj/EmployeeProjectApp1Application.java
@@ -0,0 +1,13 @@
+package com.adobe.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProjectApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProjectApp1Application.class, args);
+	}
+
+}
